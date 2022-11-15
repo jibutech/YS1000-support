fca316711328fbde3c763b457de4b571a73e8813
jerry <jerry@jibudata.com>
2022-11-15 12:47:15 +0800

    fix: update preprare script

--- offline/prepare-image.sh
+++ offline/prepare-image.sh
@@ -173,31 +173,31 @@ function exportImages {
 
 ys1000Repo=registry.cn-shanghai.aliyuncs.com/jibutech
 
-originTag=v2.9.0
+originTag=datasure-2.10.1-latest
 ys1000Images=(
     ${ys1000Repo}/qiming-operator:${originTag}
     ${ys1000Repo}/webserver:${originTag}
     ${ys1000Repo}/hookrunner:${originTag}
     ${ys1000Repo}/agent-operator:${originTag}
+    ${ys1000Repo}/velero:v1.7.0-jibu-dev-529eb463-20221109131403
+    ${ys1000Repo}/velero-restic-restore-helper:v1.7.0
+    ${ys1000Repo}/velero-plugin-for-aws:v1.3.0
+    ${ys1000Repo}/velero-plugin-for-csi:v0.2.0-jibu-b1c547a-202209050731
+    ${ys1000Repo}/velero-plugin:${originTag}
+    ${ys1000Repo}/data-mover:${originTag}
+    ${ys1000Repo}/data-verify:${originTag}
+    ${ys1000Repo}/amberapp:0.0.7
+    ${ys1000Repo}/dm-agent:${originTag}
+    ${ys1000Repo}/restic-dm:${originTag}
     ${ys1000Repo}/mig-ui:${originTag}
     ${ys1000Repo}/mig-discovery:${originTag}
     ${ys1000Repo}/mig-controller:${originTag}
     ${ys1000Repo}/cron:${originTag}
+    ${ys1000Repo}/stub:${originTag}
+    ${ys1000Repo}/hook-runner:latest
     ${ys1000Repo}/helm-tool:${originTag}
     ${ys1000Repo}/self-restore:${originTag}
-    ${ys1000Repo}/data-mover:${originTag}
-    ${ys1000Repo}/dm-agent:${originTag}
-    ${ys1000Repo}/restic-dm:${originTag}
-    ${ys1000Repo}/stub:${originTag}
-    ${ys1000Repo}/velero-plugin:${originTag}
-    ${ys1000Repo}/ys1000-offline-installer:${originTag}
     ${ys1000Repo}/log-collector:v2.7.0
-    ${ys1000Repo}/velero-restic-restore-helper:v1.7.0
-    ${ys1000Repo}/hook-runner:latest
-    ${ys1000Repo}/amberapp:0.0.6
-    ${ys1000Repo}/velero:v1.7.0-jibu-fcabaacb-202210161435
-    ${ys1000Repo}/velero-plugin-for-aws:v1.3.0
-    ${ys1000Repo}/velero-plugin-for-csi:v0.2.0-jibu-b1c547a-202209050731 
     ${ys1000Repo}/mysql:8.0.29-debian-10-r23
     ${ys1000Repo}/apiserver:v0.5.1
     ${ys1000Repo}/clustersynchro-manager:v0.5.1
